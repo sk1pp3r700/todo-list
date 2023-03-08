f2624d20c8f5d12fcca0ef0825182dac75bdb9c9
sk1pp3r700 <eandre70@gmail.com>
2023-03-08 13:45:15 +0000

    init

---
+++ app/Models/TodoItem.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+// public function group()
+// {
+//     return $this->belongsTo(Group::class);
+// }
+
+
+
+
+class TodoItem extends Model
+{
+    use HasFactory;
+}
+
---
+++ database/migrations/2023_03_06_113023_create_todo_items_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('todo_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->boolean('completed')->default(false);
+            $table->unsignedBigInteger('group_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('todo_items');
+    }
+};
